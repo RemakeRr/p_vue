f18b348086b2d1c9c37fac5e359fa7cc265b86a6
Alibek Muhumov <RemakeRr@users.noreply.github.com>
2017-08-03 17:04:36 +0600

    services section

---
+++ components/button_contact/button_contact.vue
@@ -0,0 +1,31 @@
+<template lang="pug" scoped>
+  .t-container
+    .row
+      .col-md-4
+        a(href="mailto:muxumov0087@gmail.com")
+          button Связаться со мной
+</template>
+
+<style lang="sass">
+$black: #1F1F1F
+$blue: #1789FC
+$white: #ffffff
+$grey: #777879
+button
+  width: 240px
+  height: 35px
+  background-color: $blue
+  color: $white
+  border-color: transparent
+  border-radius: 15px
+  box-shadow: 0 5px 15px 0px $blue
+  transition: all .2s
+  font-weight: 600
+  cursor: pointer
+  outline: none
+  &:hover
+    background-color: $white
+    color: $blue
+    box-shadow: 0 5px 15px 0px white
+
+</style>
